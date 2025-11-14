63613fb934e4016a8fe91ac3eee209a5dc4c1ac3
Chen-Xin-Yuan <c007xy0101@outlook.com>
2025-11-14 12:21:49 +0800

    添加源文件

---
+++ c语言课程设计（大数相乘除）/9_15/PrintData.h
@@ -0,0 +1,9 @@
+#pragma once
+#pragma once
+#ifndef __PRINTDATA_H__
+#define __PRINTDATA_H__
+
+void Print_Mutiply_Result(int* Result_Mutiply);
+void Print_Divide_Result(int* Result_Divide, int* Remainder);
+
+#endif
