4c0dcac2a0de754035a2862b2fa42b73d4d664f9
Chen-Xin-Yuan <c007xy0101@outlook.com>
2025-11-14 12:37:52 +0800

    Delete c语言课程设计（大数相乘除） directory

--- c语言课程设计（大数相乘除）/9_15/PrintData.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#pragma once
-#ifndef __PRINTDATA_H__
-#define __PRINTDATA_H__
-
-void Print_Mutiply_Result(int* Result_Mutiply);
-void Print_Divide_Result(int* Result_Divide, int* Remainder);
-
-#endif
